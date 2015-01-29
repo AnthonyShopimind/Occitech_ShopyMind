596ad55e772eb60788c06f165fcc5851953ebbbf
Pierre Martin <pierre@occitech.fr>
2015-01-29 09:40:55 +0100

    Fixed SQL setups to support Magento 1.5

--- src/app/code/local/SPM/ShopyMind/sql/shopymind_setup/upgrade-1.0.2-1.1.4.php
+++ src/app/code/local/SPM/ShopyMind/sql/shopymind_setup/upgrade-1.0.2-1.1.4.php
@@ -2,28 +2,16 @@
 $installer = $this;
 $installer->startSetup();
 $installer->getConnection()->addColumn(
-$installer->getTable('spmcartoorder'),
+        $installer->getTable('spmcartoorder'),
         'email',
-        array(
-                'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
-                'length'    => 255,
-                'unsigned'  => true,
-                'nullable'  => true,
-                'comment'   => 'Customer email'
-        )
+        "varchar(255) NULL COMMENT 'Customer email'"
 );
 $installer->getConnection()->addIndex($installer->getTable('spmcartoorder'), 'email', 'email');
 
 $installer->getConnection()->addColumn(
-$installer->getTable('spmcartoorder'),
-'voucher_number',
-array(
-        'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
-        'length'    => 20,
-        'unsigned'  => true,
-        'nullable'  => true,
-        'comment'   => 'Voucher used'
-)
+        $installer->getTable('spmcartoorder'),
+        'voucher_number',
+        "varchar(20) NULL COMMENT 'Voucher used'"
 );
 $installer->getConnection()->addIndex($installer->getTable('spmcartoorder'), 'voucher_number', 'voucher_number');
 $installer->endSetup();
