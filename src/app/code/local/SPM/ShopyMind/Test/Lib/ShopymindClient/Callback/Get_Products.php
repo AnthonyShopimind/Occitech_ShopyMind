3ad1ba6df6b3fd76e9c85e8cefb1ff864ed39d94
Hadrien <hadrien@occitech.fr>
2015-02-02 16:25:04 +0100

    Fix tests for Magento 1.5.1.0 — refs #3

--- src/app/code/local/SPM/ShopyMind/Test/Lib/ShopymindClient/Callback/Get_Products.php
+++ src/app/code/local/SPM/ShopyMind/Test/Lib/ShopymindClient/Callback/Get_Products.php
@@ -8,9 +8,10 @@ class SPM_ShopyMind_Test_Lib_ShopymindClient_Callback_Get_Products extends EcomD
 
     public function testCanGetRandomProducts()
     {
+        Mage::app()->setCurrentStore(1);
         $products = ShopymindClient_Callback::getProducts(1, false, false, true, 1);
 
-        $this->assertRegExp('#/catalog/product/view/id/[1-2]/#', $products[0]['product_url']);
+        $this->assertRegExp('#catalog/product/view/id/[1-2]/#', $products[0]['product_url']);
     }
 
     public function testIfAStoreHasNoProductsNothingIsReturned()
