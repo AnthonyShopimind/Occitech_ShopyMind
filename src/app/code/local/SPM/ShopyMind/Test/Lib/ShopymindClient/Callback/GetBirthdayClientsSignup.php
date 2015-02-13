b5eee51be66f14193e2b6c9045d8db8c1da387e7
Hadrien <hadrien@occitech.fr>
2015-02-13 17:58:28 +0100

    Use sql query from above and adapt it to this method's behavior — refs #35

--- src/app/code/local/SPM/ShopyMind/Test/Lib/ShopymindClient/Callback/GetBirthdayClientsSignup.php
+++ src/app/code/local/SPM/ShopyMind/Test/Lib/ShopymindClient/Callback/GetBirthdayClientsSignup.php
@@ -7,19 +7,19 @@ class SPM_ShopyMind_Test_Lib_ShopymindClient_Callback_GetBirthdayClientsSignup e
 {
     public function testGetBirthdayClientsSignupShoulReturnEmptyIfNoCustomersMatchFilters()
     {
-        $results = ShopymindClient_Callback::getBirthdayClientsSignUp(false, '2015-11-21 12:34:56', array());
+        $results = ShopymindClient_Callback::getBirthdayClientsSignUp(false, '2015-11-21 12:34:56', array(array('country' => 'FR'), array('country' => 'US')));
         $this->assertEmpty($results);
     }
 
     public function testGetBirthdayClientsSignupShouldNotReturnClientWhoCreatedTheirAccountAtDateReference()
     {
-        $results = ShopymindClient_Callback::getBirthdayClientsSignUp(false, '2014-10-21 12:34:56', array());
+        $results = ShopymindClient_Callback::getBirthdayClientsSignUp(false, '2014-10-21 12:34:56', array(array('country' => 'FR'), array('country' => 'US')));
         $this->assertEmpty($results);
     }
 
     public function testGetBirthdayClientsSignupShouldReturnCustomersWhoCreatedTheirAccountOneYearAgo()
     {
-        $results = ShopymindClient_Callback::getBirthdayClientsSignUp(false, '2015-10-21 12:34:56', array());
+        $results = ShopymindClient_Callback::getBirthdayClientsSignUp(false, '2015-10-21 12:34:56', array(array('country' => 'FR'), array('country' => 'US')));
         $expected = array('1', '3', '4');
         $this->assertCustomerIdsEquals($expected, $results);
     }
@@ -44,16 +44,6 @@ class SPM_ShopyMind_Test_Lib_ShopymindClient_Callback_GetBirthdayClientsSignup e
         $this->assertCustomerIdsEquals($expected, $results);
     }
 
-    public function testGetBirthdayClientsSignupShoulReturnClientInCorrectRegion()
-    {
-        $results = ShopymindClient_Callback::getBirthdayClientsSignUp(false, '2015-10-21 12:34:56', array(
-            array('region' => 'AL')
-        ));
-
-        $expected = array('3');
-        $this->assertCustomerIdsEquals($expected, $results);
-    }
-
     public function testGetBirthdayClientsSignupShoulReturnClientInCorrectRegionsAndCountries()
     {
         $results = ShopymindClient_Callback::getBirthdayClientsSignUp(false, '2015-10-21 12:34:56', array(
@@ -61,13 +51,24 @@ class SPM_ShopyMind_Test_Lib_ShopymindClient_Callback_GetBirthdayClientsSignup e
             array('country' => 'FR')
         ));
 
-        $expected = array('3');
+        $expected = array('1', '3');
+        $this->assertCustomerIdsEquals($expected, $results);
+    }
+
+    public function testGetBirthdayClientsSignupShoulReturnClientInCorrectRegionsAndCountriesAndNotHavingAddresses()
+    {
+        $results = ShopymindClient_Callback::getBirthdayClientsSignUp(false, '2015-10-21 12:34:56', array(
+            array('country' => 'US', 'region' => 'AL'),
+            false
+        ));
+
+        $expected = array('3', '6');
         $this->assertCustomerIdsEquals($expected, $results);
     }
 
     public function testGetBirthdayClientsSignupShoulReturnClientCountCorrespondingToFilters()
     {
-        $results = ShopymindClient_Callback::getBirthdayClientsSignUp(false, '2015-10-21 12:34:56', array(), true);
+        $results = ShopymindClient_Callback::getBirthdayClientsSignUp(false, '2015-10-21 12:34:56', array(array('country' => 'FR'), array('country' => 'US')), true);
 
         $expected = 3;
         $this->assertEquals($expected, $results['count']);
@@ -75,7 +76,7 @@ class SPM_ShopyMind_Test_Lib_ShopymindClient_Callback_GetBirthdayClientsSignup e
 
     public function testGetBirthdayClientsSignupShoulReturnClientAccordingToScope()
     {
-        $results = ShopymindClient_Callback::getBirthdayClientsSignUp('store-2', '2015-10-27 12:34:56', array());
+        $results = ShopymindClient_Callback::getBirthdayClientsSignUp('store-2', '2015-10-27 12:34:56', array(array('country' => 'FR'), array('country' => 'US')));
 
         $expected = array('5');
         $this->assertCustomerIdsEquals($expected, $results);
