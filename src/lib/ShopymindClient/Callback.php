674d554d7c3b9c26bdcf86013312446de4d5549c
Hadrien <hadrien@occitech.fr>
2015-05-26 17:12:21 +0200

    Fix customer data retrieval if customer region is not defined

--- src/lib/ShopymindClient/Callback.php
+++ src/lib/ShopymindClient/Callback.php
@@ -126,7 +126,7 @@ class ShopymindClient_Callback {
                   INNER JOIN `' . $tablePrefix . 'sales_flat_quote` AS `quote` ON (
                     `quote`.`entity_id` = `quote_address`.`quote_id`
                   )
-                  INNER JOIN `' . $tablePrefix . 'directory_country_region` AS `directory_country_region` ON(`directory_country_region`.`region_id` = `quote_address`.`region_id`)
+                  LEFT JOIN `' . $tablePrefix . 'directory_country_region` AS `directory_country_region` ON(`directory_country_region`.`region_id` = `quote_address`.`region_id`)
                   WHERE
                     `quote_address`.`email` IN("' . (implode('", "', (array) $idOrEmails)) . '")
                     AND `quote_address`.`address_type` = "billing"
@@ -152,7 +152,7 @@ class ShopymindClient_Callback {
                   INNER JOIN `' . $tablePrefix . 'sales_flat_order` AS `order` ON (
                     `order`.`entity_id` = `order_address`.`parent_id`
                   )
-                  INNER JOIN `' . $tablePrefix . 'directory_country_region` AS `directory_country_region` ON(`directory_country_region`.`region_id` = `order_address`.`region_id`)
+                  LEFT JOIN `' . $tablePrefix . 'directory_country_region` AS `directory_country_region` ON(`directory_country_region`.`region_id` = `order_address`.`region_id`)
                   WHERE
                     `order_address`.`email` IN("' . (implode('", "', (array) $idOrEmails)) . '")
                     AND `order_address`.`address_type` = "billing"
