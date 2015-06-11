7f3fbff3110857ebecdf66b4f086e5b22b0ae41f
Hadrien <hadrien@occitech.fr>
2015-06-11 18:17:51 +0200

    Allow tests to run with flat catalog on — refs #49
    Also set current store to a non admin one as some models (like Mage_Catalog_Model_Category) do not use flat resources in admin context

---
+++ src/app/code/local/SPM/ShopyMind/Test/Observer.php
@@ -0,0 +1,50 @@
+<?php
+
+class SPM_ShopyMind_Test_Observer
+{
+    public function beforeTestStart()
+    {
+        if (Mage::app()->getStore()->isAdmin()) {
+            $store = Mage::getModel('core/store')->load(1);
+            if (!$store->isEmpty()) {
+                $this->_setStore($store->getCode());
+            }
+        }
+
+        if ($this->_shouldRunWithFlatCatalog()) {
+            $this->_enableFlatCatalog();
+            $this->_reindexFlatCatalog();
+        }
+    }
+
+    protected function _setStoreConfig($path, $value)
+    {
+        Mage::app()->getStore()
+            ->setConfig($path, $value);
+    }
+
+    protected function _shouldRunWithFlatCatalog()
+    {
+        return getenv('USE_FLAT_CATALOG');
+    }
+
+    protected function _enableFlatCatalog()
+    {
+        $this->_setStoreConfig('catalog/frontend/flat_catalog_product', '1');
+        $this->_setStoreConfig('catalog/frontend/flat_catalog_category', '1');
+    }
+
+    protected function _reindexFlatCatalog()
+    {
+        foreach (array('catalog_product_flat', 'catalog_category_flat') as $indexer) {
+            Mage::getSingleton('index/indexer')
+                ->getProcessByCode($indexer)
+                ->reindexAll();
+        }
+    }
+
+    protected function _setStore($store)
+    {
+        EcomDev_PHPUnit_Test_Case_Util::setCurrentStore($store);
+    }
+}
