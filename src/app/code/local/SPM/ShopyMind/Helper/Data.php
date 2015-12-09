c15959c5c0a42c12202a823697a2de7bcdc65416
Adrien Louis-Rossignol <adrien@occitech.fr>
2015-12-09 11:26:59 +0100

    Correctly indent file - issue 51

--- src/app/code/local/SPM/ShopyMind/Helper/Data.php
+++ src/app/code/local/SPM/ShopyMind/Helper/Data.php
@@ -9,4 +9,35 @@
  * @version     $Id Data.php 2014-12-17$
  */
 class SPM_ShopyMind_Helper_Data extends Mage_Core_Helper_Abstract {
+
+    public function formatCustomerQuote($quote)
+    {
+        return array(
+            'id_customer' => $quote->getCustomerId(),
+            'id_cart' => $quote->getId(),
+            'date_add' => $quote->getCreatedAt(),
+            'date_upd' => $quote->getUpdatedAt(),
+            'amount' => $quote->getGrandTotal(),
+            'tax_rate',
+            'currency' => $quote->getQuoteCurrencyCode(),
+            'voucher_used' => ShopymindClient_Callback::getCartVouchers(),
+            'voucher_amount',
+            'products',
+        );
+    }
+
+    public function formatQuoteItem($quoteItem)
+    {
+        $product = $quoteItem->getProduct();
+        $children = $quoteItem->getChildren();
+        $combinationId = count($children) ? $children[0]->getProductId() : $product->getId();
+
+        return array(
+            'id_product' => $product->getId(),
+            'qty' => $quoteItem->getQty(),
+            'price' => $quoteItem->getPriceInclTax(),
+            'id_combination' => $combinationId,
+            'id_manufacturer' => $product->getManufacturer(),
+        );
+    }
 }
