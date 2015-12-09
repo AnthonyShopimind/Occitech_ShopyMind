d0076bb25e7e7b9f8acd23e68935b2d63322e4ae
Adrien Louis-Rossignol <adrien@occitech.fr>
2015-12-09 16:47:49 +0100

    Create DataMapper for quoteItem issue - 63

---
+++ src/lib/ShopymindClient/DataMapper/QuoteItem.php
@@ -0,0 +1,20 @@
+<?php
+
+class ShopymindClient_DataMapper_QuoteItem
+{
+
+    public function format(Mage_Sales_Model_Quote_Item $quoteItem)
+    {
+        $product = $quoteItem->getProduct();
+        $children = $quoteItem->getChildren();
+        $combinationId = count($children) ? $children[0]->getProductId() : $product->getId();
+
+        return array(
+            'id_product' => $product->getId(),
+            'id_manufacturer' => $combinationId,
+            'id_combination' => $product->getManufacturer(),
+            'price' => $quoteItem->getPriceInclTax(),
+            'qty' => $quoteItem->getQty(),
+        );
+    }
+}
