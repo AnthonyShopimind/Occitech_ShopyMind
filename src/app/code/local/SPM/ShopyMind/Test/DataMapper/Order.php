956f9c810e6d443909de2ed92e28c4bd9068eec5
Adrien Louis-Rossignol <adrien@occitech.fr>
2015-12-11 14:22:32 +0100

    Add test for voucher used issue - 63

--- src/app/code/local/SPM/ShopyMind/Test/DataMapper/Order.php
+++ src/app/code/local/SPM/ShopyMind/Test/DataMapper/Order.php
@@ -42,4 +42,37 @@ class SPM_ShopyMind_Test_Lib_ShopymindClient_DataMapper_Order extends EcomDev_PH
         $actual = $this->SUT->format($order, array(), null);
         $this->assertEquals($expected, $actual);
     }
+
+    /**
+     * @loadFixture coupon
+     */
+    public function testFormatOrderDataWithCoupon()
+    {
+        $order = Mage::getModel('sales/order')->load(2);
+        $quote = Mage::getModel('sales/quote')->load(2);
+        $order->setQuote($quote);
+
+        $expected = array(
+            'shop_id_shop' => 1,
+            'order_is_confirm' => true,
+            'order_reference' => 10002,
+            'id_cart' => 2,
+            'id_status' => 'pending',
+            'date_cart' => '2015-12-09 13:53:06',
+            'id_order' => 2,
+            'lang' => 'fr',
+            'amount' => 28.0300,
+            'tax_rate' => 1.0000,
+            'currency' => 'EUR',
+            'date_order' => '2015-12-09 11:53:06',
+            'voucher_used' => array("ML9DUOQZLISN"),
+            'voucher_amount' => -10,
+            'products' => array(),
+            'customer' => array(),
+            'shipping_number' => null,
+        );
+
+        $actual = $this->SUT->format($order, array(), null);
+        $this->assertEquals($expected, $actual);
+    }
 }
