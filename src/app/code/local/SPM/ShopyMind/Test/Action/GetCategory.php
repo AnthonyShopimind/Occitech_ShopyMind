157c00bad04e56e35f36495f586f2413e0a12671
Damien Biasotto <damien@occitech.fr>
2015-12-14 17:40:58 +0100

    Added test with unrestricted scope. refs #61

--- src/app/code/local/SPM/ShopyMind/Test/Action/GetCategory.php
+++ src/app/code/local/SPM/ShopyMind/Test/Action/GetCategory.php
@@ -87,4 +87,35 @@ class SPM_ShopyMind_Test_Action_GetCategory extends EcomDev_PHPUnit_Test_Case
         $category = $Action->process(false);
         $this->assertEquals($expected, $category->getData());
     }
+
+    /**
+     * @loadFixture default
+     */
+    public function testProcessWithUndefinedScope()
+    {
+        $scope = SPM_ShopyMind_Model_Scope::fromShopymindId('');
+        $Action = new SPM_ShopyMind_Action_GetCategory($scope, 6);
+
+        $expected = array(
+            'entity_id' => 6,
+            'parent_id' => 5,
+            'name' => 'Hello world',
+            'description' => 'this is REALLY a test category',
+            'url_key' => 'hello-world',
+            'created_at' => '2013-10-26 12:00:00',
+            'is_active' => 1,
+            'entity_type_id' => 3,
+            'attribute_set_id' => 3,
+            'updated_at' => '2013-10-26 13:00:00',
+            'path' => '1/5/6',
+            'position' => '1',
+            'locale' => 'en_US',
+            'level' => 1,
+            'children_count' => 0,
+            'include_in_menu' => 1
+        );
+
+        $category = $Action->process(false);
+        $this->assertEquals($expected, $category->getData());
+    }
 }
