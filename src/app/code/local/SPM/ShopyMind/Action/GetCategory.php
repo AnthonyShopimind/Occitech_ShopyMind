cb40304ecffc011492c7240f87f4a0609f7d0f67
Damien Biasotto <damien@occitech.fr>
2015-12-15 10:21:33 +0100

    Removed superfluous arguments

--- src/app/code/local/SPM/ShopyMind/Action/GetCategory.php
+++ src/app/code/local/SPM/ShopyMind/Action/GetCategory.php
@@ -34,7 +34,7 @@ class SPM_ShopyMind_Action_GetCategory implements SPM_ShopyMind_Interface_Action
         $this->Formatter = new SPM_ShopyMind_DataMapper_Category($category);
         $locale = $this->scope->getConfig('general/locale/code');
         $category->setData('locale', (string) $locale);
-        $data = $formatData ? $this->Formatter->format($category) : $category;
+        $data = $formatData ? $this->Formatter->format() : $category;
         $appEmulation->stopEnvironmentEmulation($emulatedEnvironment);
 
         return $data;
