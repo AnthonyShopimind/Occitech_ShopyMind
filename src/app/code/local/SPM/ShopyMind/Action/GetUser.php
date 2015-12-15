7d5e24759a644e06f81acecf387b8ebe8bb9220a
Adrien Louis-Rossignol <adrien@occitech.fr>
2015-12-15 11:14:11 +0100

    Extract getUser into an action issue #52

---
+++ src/app/code/local/SPM/ShopyMind/Action/GetUser.php
@@ -0,0 +1,146 @@
+<?php
+
+class SPM_ShopyMind_Action_GetUser implements SPM_ShopyMind_Interface_Action
+{
+    private $params;
+
+    public function __construct($idOrEmails, $fromQuotes = false)
+    {
+        $this->params['identifier'] = $idOrEmails;
+        $this->params['fromQuote'] = $fromQuotes;
+    }
+
+    public function process()
+    {
+        $users = $this->fetchUsers();
+        $return = array_map(array('SPM_ShopyMind_DataMapper_Customer', 'format'), $users);
+
+        return (sizeof($return) === 1 ? $return [0] : $return);
+    }
+
+    public function fetchUsers()
+    {
+        $tablePrefix = Mage::getConfig()->getTablePrefix();
+        $resource = Mage::getSingleton('core/resource');
+        $readConnection = $resource->getConnection('core_read');
+
+        if (is_numeric($this->params['identifier'])) {
+            $query = '
+                SELECT
+                    `customer_default_phone`.`value` as `phone`,
+                    `customer_default_billing_country`.`value` as `country_code`,
+                    `customer_default_billing_state`.`code` as `region_code`,
+                    `customer_default_billing_postcode`.`value` as `postcode`,
+                    `customer_primary_table`.`entity_id`,
+                    `customer_primary_table`.`store_id`,
+                    `customer_firstname_table`.`value` as `firstname`,
+                    `customer_lastname_table`.`value` as `lastname`,
+                    `customer_primary_table`.`email`,
+                    `customer_primary_table`.`created_at`,
+                    `customer_primary_table`.`group_id`,
+                    `customer_gender`.`value` AS `gender_id`,
+                    `customer_birth_table`.`value` AS `birthday`
+                FROM `' . $tablePrefix . 'customer_entity` AS `customer_primary_table`
+                LEFT JOIN `' . $tablePrefix . 'customer_entity_datetime` AS `customer_birth_table`
+                ON (
+                    `customer_birth_table`.`entity_id` = `customer_primary_table`.`entity_id`
+                    AND `customer_birth_table`.`attribute_id` = ' . ShopymindClient_Callback::getMagentoAttributeCode('customer', 'dob') . '
+                )
+                LEFT JOIN `' . $tablePrefix . 'customer_entity_varchar` AS `customer_firstname_table`
+                ON
+                    (`customer_firstname_table`.`entity_id` = `customer_primary_table`.`entity_id`)
+                    AND (`customer_firstname_table`.`attribute_id` = ' . ShopymindClient_Callback::getMagentoAttributeCode('customer', 'firstname') . ')
+                LEFT JOIN `' . $tablePrefix . 'customer_entity_varchar` AS `customer_lastname_table`
+                ON
+                    (`customer_lastname_table`.`entity_id` = `customer_primary_table`.`entity_id`)
+                    AND (`customer_lastname_table`.`attribute_id` = ' . ShopymindClient_Callback::getMagentoAttributeCode('customer', 'lastname') . ')
+                LEFT JOIN `' . $tablePrefix . 'customer_entity_int` AS `customer_default_billing_jt`
+                ON
+                    (`customer_default_billing_jt`.`entity_id` = `customer_primary_table`.`entity_id`)
+                    AND (`customer_default_billing_jt`.`attribute_id` = ' . ShopymindClient_Callback::getMagentoAttributeCode('customer', 'default_shipping') . ')
+                LEFT JOIN `' . $tablePrefix . 'customer_address_entity_varchar` AS `customer_default_billing_country`
+                ON
+                    (`customer_default_billing_jt`.`value` = `customer_default_billing_country`.`entity_id`)
+                    AND (`customer_default_billing_country`.`attribute_id` = ' . ShopymindClient_Callback::getMagentoAttributeCode('customer_address', 'country_id') . ')
+                LEFT JOIN `' . $tablePrefix . 'customer_address_entity_varchar` AS `customer_default_billing_postcode`
+                ON
+                    (`customer_default_billing_jt`.`value` = `customer_default_billing_postcode`.`entity_id`)
+                    AND (`customer_default_billing_postcode`.`attribute_id` = ' . ShopymindClient_Callback::getMagentoAttributeCode('customer_address', 'postcode') . ')
+                LEFT JOIN `' . $tablePrefix . 'customer_address_entity_varchar` AS `customer_default_phone`
+                ON
+                    (`customer_default_billing_jt`.`value` = `customer_default_phone`.`entity_id`)
+                    AND (`customer_default_phone`.`attribute_id` = ' . ShopymindClient_Callback::getMagentoAttributeCode('customer_address', 'telephone') . ')
+                LEFT JOIN `' . $tablePrefix . 'customer_address_entity_int` AS `customer_default_billing_state_jt`
+                ON
+                    (`customer_default_billing_country`.`entity_id` = `customer_default_billing_state_jt`.`entity_id`)
+                    AND (
+                        `customer_default_billing_state_jt`.`attribute_id` = ' . ShopymindClient_Callback::getMagentoAttributeCode('customer_address', 'region_id') . '
+                        OR `customer_default_billing_state_jt`.`attribute_id` IS NULL
+                    )
+                LEFT JOIN `' . $tablePrefix . 'directory_country_region` AS `customer_default_billing_state`
+                ON (`customer_default_billing_state`.`region_id` = `customer_default_billing_state_jt`.`value`)
+                LEFT JOIN `' . $tablePrefix . 'customer_entity_int` AS `customer_gender`
+                ON
+                    (`customer_gender`.`entity_id` = `customer_primary_table`.`entity_id`)
+                    AND (`customer_gender`.`attribute_id` = ' . ShopymindClient_Callback::getMagentoAttributeCode('customer', 'gender') . ')
+
+                WHERE  `customer_primary_table`.`entity_id` IN(' . (implode(', ', (array)$this->params['identifier'])) . ')
+                GROUP BY `customer_primary_table`.`entity_id`';
+        } else {
+            if ($this->params['fromQuote']) {
+                $query = '
+                  SELECT
+                    `quote_address`.`telephone` as `phone`,
+                    `quote_address`.`country_id` as `country_code`,
+                    `directory_country_region`.`code` as `region_code`,
+                    `quote_address`.`postcode`,
+                    `quote_address`.`email` AS `entity_id`, `quote`.`store_id`,
+                    `quote_address`.`firstname`,
+                    `quote_address`.`lastname`,
+                    `quote_address`.`email`,
+                    `quote`.`created_at`,
+                    `quote`.`customer_group_id` AS `group_id`,
+                    `quote`.`customer_gender` AS `gender_id`,
+                    `quote`.`customer_dob` AS `birthday`
+                  FROM `' . $tablePrefix . 'sales_flat_quote_address` AS `quote_address`
+                  INNER JOIN `' . $tablePrefix . 'sales_flat_quote` AS `quote` ON (
+                    `quote`.`entity_id` = `quote_address`.`quote_id`
+                  )
+                  LEFT JOIN `' . $tablePrefix . 'directory_country_region` AS `directory_country_region` ON(`directory_country_region`.`region_id` = `quote_address`.`region_id`)
+                  WHERE
+                    `quote_address`.`email` IN("' . (implode('", "', (array)$this->params['identifier'])) . '")
+                    AND `quote_address`.`address_type` = "billing"
+                  GROUP BY `quote_address`.`email`
+                  ORDER BY `quote`.`entity_id` ASC';
+            } else {
+                $query = '
+                  SELECT
+                    `order_address`.`telephone` as `phone`,
+                    `order_address`.`country_id` as `country_code`,
+                    `directory_country_region`.`code` as `region_code`,
+                    `order_address`.`postcode`,
+                    `order_address`.`email` AS `entity_id`,
+                    `order`.`store_id`,
+                    `order_address`.`firstname`,
+                    `order_address`.`lastname`,
+                    `order_address`.`email`,
+                    `order`.`created_at`,
+                    `order`.`customer_group_id` AS `group_id`,
+                    `order`.`customer_gender` AS `gender_id`,
+                    `order`.`customer_dob` AS `birthday`
+                  FROM `' . $tablePrefix . 'sales_flat_order_address` AS `order_address`
+                  INNER JOIN `' . $tablePrefix . 'sales_flat_order` AS `order` ON (
+                    `order`.`entity_id` = `order_address`.`parent_id`
+                  )
+                  LEFT JOIN `' . $tablePrefix . 'directory_country_region` AS `directory_country_region` ON(`directory_country_region`.`region_id` = `order_address`.`region_id`)
+                  WHERE
+                    `order_address`.`email` IN("' . (implode('", "', (array)$this->params['identifier'])) . '")
+                    AND `order_address`.`address_type` = "billing"
+                  GROUP BY `order_address`.`email`
+                  ORDER BY `order`.`entity_id` ASC';
+            }
+        }
+
+        return $readConnection->fetchAll($query);
+    }
+}
--- src/app/code/local/SPM/ShopyMind/DataMapper/Customer.php
+++ src/app/code/local/SPM/ShopyMind/DataMapper/Customer.php
@@ -2,8 +2,31 @@
 
 class SPM_ShopyMind_DataMapper_Customer
 {
-    public function format($customerId)
+    public function format($user)
     {
-        return ShopymindClient_Callback::getUser($customerId);
+        $subscriber = Mage::getModel('newsletter/subscriber')->loadByEmail($user['email']);
+
+        return array (
+            'id_customer' => $user['entity_id'],
+            'shop_id_shop' => $user['store_id'],
+            'optin' => $subscriber->isSubscribed(),
+            'customer_since' => $user['created_at'],
+            'last_name' => $user['lastname'],
+            'first_name' => $user['firstname'],
+            'email_address' => $user['email'],
+            'phone1' => (isset($user['phone']) && $user['phone'] ? $user['phone'] : ''),
+            'phone2' => '',
+            'gender' => (isset($user['gender_id']) && ($user['gender_id'] == 1 || $user['gender_id'] == 2) ? $user['gender_id'] : 0),
+            'birthday' => (isset($user['birthday']) ? $user['birthday'] : 0),
+            'locale' => ShopymindClient_Callback::getUserLocale($user['entity_id'], $user['store_id'], $user['country_code']),
+            'region' => $user['region_code'],
+            'postcode' => $user['postcode'],
+            'date_last_order' => ShopymindClient_Callback::getDateLastOrder($user['entity_id']),
+            'nb_order' => ShopymindClient_Callback::countCustomerOrder($user['entity_id'], null),
+            'sum_order' => ShopymindClient_Callback::sumCustomerOrder($user['entity_id']),
+            'nb_order_year' => ShopymindClient_Callback::countCustomerOrder($user['entity_id'], '1 YEAR'),
+            'sum_order_year' => ShopymindClient_Callback::sumCustomerOrder($user['entity_id'], '1 YEAR'),
+            'groups' => array ($user['group_id']),
+        );
     }
 }
