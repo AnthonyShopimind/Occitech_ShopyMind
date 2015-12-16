1925f799919e3880f816f8a0d06399320ba2b189
Damien Biasotto <damien@occitech.fr>
2015-12-16 09:34:07 +0100

    Made syncProductsCategories method static.

--- src/lib/ShopymindClient/Callback.php
+++ src/lib/ShopymindClient/Callback.php
@@ -2128,7 +2128,7 @@ class ShopymindClient_Callback {
         return $SyncCustomersAction->process();
     }
 
-    public function syncProductsCategories($id_shop, $start, $limit, $lastUpdate, $idCategory = false, $justCount = false)
+    public static function syncProductsCategories($id_shop, $start, $limit, $lastUpdate, $idCategory = false, $justCount = false)
     {
         if (class_exists('ShopymindClient_CallbackOverride', false) && method_exists('ShopymindClient_CallbackOverride', __FUNCTION__)) {
             return call_user_func_array(array (
