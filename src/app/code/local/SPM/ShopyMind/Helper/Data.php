AnthonyShopimind/Occitech_ShopyMind

commit 7ff557993cfe517824a04f432b29f7dc992e100a

Implement combination fetching for configurable products - refs #53

src/app/code/local/SPM/ShopyMind/Helper/Data.php
@@ -40,13 +40,21 @@ class SPM_ShopyMind_Helper_Data extends Mage_Core_Helper_Abstract
         return date('Y-m-d H:i:s', strtotime($date));
     }
 
-    public function formatCombinationsOfProduct(Mage_Catalog_Model_Product $product, $formatter, $attributeIds = null)
+    public function formatCombinationsOfProduct(Mage_Catalog_Model_Product $product, $formatter, $attributeNames = null)
     {
         $combinations = array();
         if ($product->getTypeId() === Mage_Catalog_Model_Product_Type_Configurable::TYPE_CODE) {
+            $ids = Mage::getModel('catalog/product_type_configurable')->getChildrenIds($product->getId());
+            $childProducts = Mage::getModel('catalog/product')
+                ->getCollection()
+                ->addAttributeToFilter('entity_id', $ids);
+            if (!empty($attributeNames)) {
+                $childProducts->addAttributeToSelect($attributeNames);
+            }
+
             $combinations = array_map(
                 $formatter,
-                Mage::getModel('catalog/product_type_configurable')->getUsedProducts($attributeIds, $product)
+                iterator_to_array($childProducts->getIterator())
             );
         }
         return $combinations;

src/app/code/local/SPM/ShopyMind/DataMapper/Product.php
@@ -18,22 +18,20 @@ class SPM_ShopyMind_DataMapper_Product
             return array();
         }
 
-        $shopymindData = array(
-            'id_product' => $product->getId(),
-            'reference' => $product->getSku(),
-            'name' => $product->getName(),
-            'description_short' => $product->getShortDescription(),
-            'description' => $product->getDescription(),
-            'product_link' => $this->helper->productUrlOf($product),
-            'image_link' => $this->helper->productImageUrlOf($product),
-            'combinations' => $this->formattedCombinationsOf($product),
-            'id_categories' => $product->getCategoryIds(),
-            'id_manufacturer' => $this->helper->manufacturerIdOf($product),
-            'price' => $product->getPrice(),
-            'price_discount' => $product->getFinalPrice(),
-            'quantity_remaining' => $product->getStockItem()->getQty(),
-            'date_creation' => $this->helper->formatDateTime($product->getCreatedAt()),
-            'active' => $product->getIsSalable(),
+        $shopymindData = array_merge(
+            $this->formatProductCommonData($product),
+            array(
+                'id_product' => $product->getId(),
+                'name' => $product->getName(),
+                'description_short' => $product->getShortDescription(),
+                'description' => $product->getDescription(),
+                'combinations' => $this->formattedCombinationsOf($product),
+                'id_categories' => $product->getCategoryIds(),
+                'id_manufacturer' => $this->helper->manufacturerIdOf($product),
+                'quantity_remaining' => $product->getStockItem()->getQty(),
+                'date_creation' => $this->helper->formatDateTime($product->getCreatedAt()),
+                'active' => $product->getIsSalable(),
+            )
         );
 
         return $shopymindData;
@@ -41,11 +39,32 @@ class SPM_ShopyMind_DataMapper_Product
 
     private function formattedCombinationsOf(Mage_Catalog_Model_Product $product)
     {
-        $formatter = function ($childProduct) {
-            return array(
-                'id' => $childProduct->getId()
+        $attributeNames = array('name', 'stock_item', 'price', 'final_price');
+
+        $commonDataFormatter = array($this, 'formatProductCommonData');
+        $formatter = function (Mage_Catalog_Model_Product $childProduct) use ($commonDataFormatter) {
+            $stockItemModel = Mage::getModel('cataloginventory/stock_item');
+            return array_merge(
+                call_user_func($commonDataFormatter, $childProduct),
+                array(
+                    'combination_name' => $childProduct->getName(),
+                    'id_combination' => $childProduct->getId(),
+                    'values' => '', // valeurs de la déclinaison
+                    'quantity_remaining' => $stockItemModel->loadByProduct($childProduct)->getQty(),
+                )
             );
         };
-        return $this->helper->formatCombinationsOfProduct($product, $formatter);
+        return $this->helper->formatCombinationsOfProduct($product, $formatter, $attributeNames);
+    }
+
+    public function formatProductCommonData(Mage_Catalog_Model_Product $product)
+    {
+        return array(
+            'reference' => $product->getSku(),
+            'product_link' => $this->helper->productUrlOf($product),
+            'image_link' => $this->helper->productImageUrlOf($product),
+            'price' => $product->getPrice(),
+            'price_discount' => $product->getFinalPrice(),
+        );
     }
 }

src/app/code/local/SPM/ShopyMind/Test/DataMapper/Product.php
@@ -75,4 +75,32 @@ QUERY
     {
         $this->markTestIncomplete('TODO');
     }
+
+    /**
+     * @ loadFixture
+     * TODO Make me pass faster
+     */
+    public function testFormattingConfigurableProductReturnsCombinationsData()
+    {
+        $this->markTestIncomplete('TODO Make it faster and stable, I stumbled upon setting it up properly in a reasonable time');
+        $configurableProduct = Mage::getModel('catalog/product')->load(2);
+        $actual = $this->SUT->format($configurableProduct);
+
+        $expectedCombinations = array (
+            array(
+                'reference' => 'sku43-configurable-pr',
+                'product_link' => 'http://shopymind.test/catalog/product/view/id/1/s/first-simple-product/',
+                'image_link' => 'http://shopymind.test/media/catalog/product/cache/1/small_image/200x200/9df78eab33525d08d6e5fb8d27136e95/images/catalog/product/placeholder/small_image.jpg',
+                'combination_name' => 'First simple product',
+                'id_combination' => 1,
+                'values' => '',
+                'price' => 13.00,
+                'price_discount' => 13.00,
+                'quantity_remaining' => 100,
+            )
+        );
+
+        $this->assertEquals($expectedCombinations, $actual['combinations']);
+    }
+
 }

src/app/code/local/SPM/ShopyMind/Test/DataMapper/Product/fixtures/default.yaml
@@ -91,6 +91,7 @@ eav:
      url_path: "first-simple-product.html"
      small_image: "/m/s/msj000t_1_not_existing.jpg"
      created_at: "2013-03-05 05:48:12"
+     color: white
      stock:
       qty: 100.00
       is_in_stock: 1
@@ -105,3 +106,21 @@ eav:
       b2b:
         name: Premier super produit
         status: 0
+
+tables:
+  eav_attribute_option:
+    - option_id: 3
+      attribute_id: 92
+      sort_order: 0
+    - option_id: 4
+      attribute_id: 92
+      sort_order: 0
+  eav_attribute_option_value:
+    - value_id: 3
+      option_id: 3
+      store_id: 0
+      value: black
+    - value_id: 4
+      option_id: 4
+      store_id: 0
+      value: white

src/app/code/local/SPM/ShopyMind/Test/DataMapper/Product/fixtures/testFormattingConfigurableProductReturnsCombinationsData.yaml
@@ -1,0 +1,30 @@
+eav:
+  catalog_product:
+   - entity_id: 2
+     name: First configurable product
+     sku: sku43-configurable-pr
+     type_id: configurable
+     attribute_set_id: 4
+     status: 1
+     visibility: 4
+     short_description: "Configurable short description"
+     description: Configurable long description
+     url_key: "first-configurable-product"
+     url_path: "first-configurable-product.html"
+     small_image: "/a/b/ab_2_not_existing.jpg"
+     created_at: "2014-03-05 05:48:12"
+     has_options: 1
+     required_options: 1
+     price: 12.00
+     stock:
+      is_in_stock: 1
+     super_attributes:
+       - color
+     configurable_children:
+       - 1
+     category_ids:
+      - 1
+      - 2
+     website_ids:
+      - b2c
+      - b2b

src/lib/ShopymindClient/Callback.php
@@ -1924,18 +1924,13 @@ class ShopymindClient_Callback {
 
     private static function combinationsOfProduct(Mage_Catalog_Model_Product $product)
     {
-        static $nameAttributeId = null;
-        if (is_null($nameAttributeId)) {
-            $nameAttributeId = Mage::getModel('eav/entity_attribute')->loadByCode(Mage_Catalog_Model_Product::ENTITY, 'name')->getId();
-        }
-
         $formatter = function ($childProduct) {
             return array(
                 'id' => $childProduct->getId(),
                 'name' => $childProduct->getName()
             );
         };
-        return Mage::helper('shopymind')->formatCombinationsOfProduct($product, $formatter, array($nameAttributeId));
+        return Mage::helper('shopymind')->formatCombinationsOfProduct($product, $formatter, array('name'));
     }
 
     public static function findCategories($id_shop, $lang = false, $search) {

commit 6b7b3c05e06cfc9f85dd201ecc42bdf349398e0d

Fix regression in combination key names
introduced in 7ff557993cfe517824a04f432b29f7dc992e100a

src/app/code/local/SPM/ShopyMind/Helper/Data.php
@@ -60,11 +60,10 @@ class SPM_ShopyMind_Helper_Data extends Mage_Core_Helper_Abstract
             if (!empty($attributeNames)) {
                 $childProducts->addAttributeToSelect($attributeNames);
             }
-
-            $combinations = array_map(
+            $combinations = array_values(array_map(
                 $formatter,
                 iterator_to_array($childProducts->getIterator())
-            );
+            ));
         }
         return $combinations;
     }