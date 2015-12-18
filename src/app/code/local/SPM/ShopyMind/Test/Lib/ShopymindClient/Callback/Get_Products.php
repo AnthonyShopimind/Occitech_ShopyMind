3cf71e6800aad1adaefe784942e161066703b391
Damien Biasotto <damien@occitech.fr>
2015-12-18 10:32:13 +0100

    Remove uneeded app store modification since there is app emulation.

--- src/app/code/local/SPM/ShopyMind/Test/Lib/ShopymindClient/Callback/Get_Products.php
+++ src/app/code/local/SPM/ShopyMind/Test/Lib/ShopymindClient/Callback/Get_Products.php
@@ -14,12 +14,9 @@ class SPM_ShopyMind_Test_Lib_ShopymindClient_Callback_Get_Products extends EcomD
 
     public function testCanGetRandomProducts()
     {
-        Mage::app()->setCurrentStore(1);
         $products = ShopymindClient_Callback::getProducts('store-1', false, false, true, 1);
 
         $this->assertRegExp('#catalog/product/view/id/[1-2]/#', $products[0]['product_link']);
-
-        Mage::app()->setCurrentStore(0);
     }
 
     public function testIfAStoreHasNoProductsNothingIsReturned()
