e7dc10441484a465acba78bc916704939f20009d
Pierre Martin <pierre@occitech.fr>
2016-12-19 17:07:51 +0100

    Update failing test according to change in 323f658acd53c4170abc490f5f18e3666ace1006

--- src/app/code/local/SPM/ShopyMind/Test/Model/Observer.php
+++ src/app/code/local/SPM/ShopyMind/Test/Model/Observer.php
@@ -158,7 +158,7 @@ class SPM_ShopyMind_Test_Model_Observer extends EcomDev_PHPUnit_Test_Case_Config
             'catalog_category_save_commit_after' => 'saveProductCategory',
             'catalog_category_delete_after' => 'deleteProductCategory',
             'customer_save_commit_after' => 'saveCustomer',
-            'customer_address_save_after' => 'saveCustomerAddress',
+            'customer_address_save_commit_after' => 'saveCustomerAddress',
             'customer_address_delete_after' => 'saveCustomerAddress',
             'customer_delete_after' => 'deleteCustomer',
         );
